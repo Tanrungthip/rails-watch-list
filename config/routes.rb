4a0f04cb3b2c6acfaed1f90d8b0a85191439d26f
Rungthip Chaothayee <ttann.trc@gmail.com>
2023-02-16 14:24:53 +0900

    list all the lists/add a new lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+	end
+  resources :bookmarks, only: %i[destroy]
+
 end
