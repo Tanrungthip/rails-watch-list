5d8a62f14d459c84ae61599e4f44848754c8c115
Rungthip Chaothayee <ttann.trc@gmail.com>
2023-02-17 16:04:49 +0900

    add poster to movie

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-  resources :lists, only: %i[index show new create] do
+  resources :lists, only: %i[index show new create destroy] do
     resources :bookmarks, only: %i[new create]
 	end
   resources :bookmarks, only: %i[destroy]
